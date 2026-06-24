066b867d50ffaa27fc1a45fcc44cdab6c9e45cec
LucaMawyin <lucamawyin@gmail.com>
2026-06-24 02:04:45 -0400

    validation set created

--- train_new_model.py
+++ train_new_model.py
@@ -12,13 +12,13 @@ def main():
         workers=8, # i5 13600kf 14C 20T
         batch=16,
         project=r"C:\Users\lucam\Desktop\Code\blackjack\runs", # Explicitly state dir (saving to wrong dir otherwise) 
-        name="card_train"
+        name="new_train"
     )
 
     model.val(
         conf=0.25,
         project=r"C:\Users\lucam\Desktop\Code\blackjack\runs",
-        name="card_val"
+        name="new_val"
     )
 
 if __name__ == "__main__":
